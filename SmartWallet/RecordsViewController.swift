156b10faeb30250e4830a69e8e0518e5e00f45b4
Soheil Novinfard <novinfard@gmail.com>
2018-01-22 00:54:52 +0000

    Add inital storyboard and model

---
+++ SmartWallet/RecordsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecordsViewController.swift
+//  SmartWallet
+//
+//  Created by Soheil on 21/01/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
