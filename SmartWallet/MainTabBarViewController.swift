e016ec648e37a9d316e83214b556680767ac13fa
Soheil Novinfard <novinfard@gmail.com>
2018-01-23 22:36:29 +0000

    Add Record Table

---
+++ SmartWallet/MainTabBarViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabBarViewController.swift
+//  SmartWallet
+//
+//  Created by Soheil on 23/01/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+	
+	var freshLaunch = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		if freshLaunch == true {
+			freshLaunch = false
+			self.selectedIndex = 1 // second tab
+		}
+	}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SmartWallet/RecordsViewController.swift
+++ SmartWallet/RecordsViewController.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 
-class RecordsViewController: UIViewController {
+class RecordsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		print("yes")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
 
 }
