addd3913e21d5f8be140e80c1810733d9f1baf4c
Soheil Novinfard <novinfard@gmail.com>
2019-06-09 23:31:41 +0100

    Added FastLane snapshot UI test to capture related pages

--- SmartWalletUITests/SmartWalletUITests.swift
+++ SmartWalletUITests/SmartWalletUITests.swift
@@ -7,22 +7,57 @@
 //
 
 import XCTest
+@testable import SmartWallet
 
 class SmartWalletUITests: XCTestCase {
 
     override func setUp() {
+		super.setUp()
+		continueAfterFailure = false
+
 		let app = XCUIApplication()
 		setupSnapshot(app)
 		app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+		super.tearDown()
+	}
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testTakingSnapshot() {
+		let app = XCUIApplication()
+		sleep(30)
 
+		let startButton = app.buttons["Start Now!"]
+		if startButton.exists {
+			snapshot("1- Splash", timeWaitingForIdle: 10)
+			startButton.tap()
+		}
+
+		snapshot("3- Records", timeWaitingForIdle: 10)
+		app.tables.cells.element(boundBy: 0).tap()
+		app.buttons["Edit"].tap()
+		snapshot("2- Add Edit Record", timeWaitingForIdle: 10)
+		app.navigationBars["Add Record"].buttons["Record"].tap()
+		app.navigationBars["Record"].buttons["Records"].tap()
+
+		let tabBarsQuery = app.tabBars
+
+		tabBarsQuery.buttons["Budget"].tap()
+		snapshot("4- Budget", timeWaitingForIdle: 10)
+
+		tabBarsQuery.buttons["Settings"].tap()
+
+		let tablesQuery = app.tables
+		tablesQuery.staticTexts["Categories"].tap()
+		snapshot("7- Categories", timeWaitingForIdle: 10)
+
+		app.navigationBars["Categories"].buttons["Settings"].tap()
+
+		tabBarsQuery.buttons["Dashboard"].tap()
+		snapshot("5- Dashboard", timeWaitingForIdle: 10)
+
+		app.swipeUp()
+		snapshot("6- Dashboard Bottom", timeWaitingForIdle: 10)
+    }
 }
