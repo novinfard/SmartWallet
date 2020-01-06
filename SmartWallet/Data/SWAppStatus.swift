7baebd4079d3c5201cb2e72424e2a59d9d39d58a
Soheil Novinfard <novinfard@gmail.com>
2020-01-06 00:38:05 +0000

    Added app update indicator and update procedure

--- SmartWallet/Data/SWAppStatus.swift
+++ SmartWallet/Data/SWAppStatus.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+enum SWAppUpdate {
+	case unknown
+	case noUpdate(current: String)
+	case updated(old: String, current: String)
+
+	static var status: SWAppUpdate {
+		let defaults = UserDefaults.standard
+
+		guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+			return .unknown
+		}
+
+		let previousVersionMayNil = defaults.string(forKey: "appVersion")
+		defaults.set(currentVersion, forKey: "appVersion")
+
+		guard let previousVersion = previousVersionMayNil else {
+			return .updated(old: "0", current: currentVersion)
+		}
+
+		if previousVersion == currentVersion {
+			return .noUpdate(current: currentVersion)
+		} else {
+			return .updated(old: previousVersion, current: currentVersion)
+		}
+	}
+}
