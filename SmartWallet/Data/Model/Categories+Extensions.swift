49775fedb9068568153e6934a4ee32cb5baabe71
Soheil Novinfard <novinfard@gmail.com>
2020-03-27 23:13:41 +0000

    Updated category's icon to use FontAwesome

--- SmartWallet/Data/Model/Categories+Extensions.swift
+++ SmartWallet/Data/Model/Categories+Extensions.swift
@@ -8,11 +8,27 @@
 
 import CoreData
 import UIKit
+import FontAwesome
+
+enum SWIconConfig {
+	static let style: FontAwesomeStyle = .solid
+	static let color: UIColor = .black
+	static let defaultSize = CGSize(width: 512, height: 512)
+}
 
 extension Categories {
-	var iconImage: UIImage? {
-		let defaultIcon = self.direction > 0 ? "UpIcon" : "DownIcon"
-		let iconName = self.icon.isEmpty ? defaultIcon : self.icon
-		return UIImage(named: iconName)
+	func iconImage(size: CGSize = SWIconConfig.defaultSize) -> UIImage? {
+		guard !self.icon.isEmpty,
+			let font = FontAwesome(rawValue: self.icon) else {
+			let defaultIcon = self.direction > 0 ? "UpIcon" : "DownIcon"
+			return UIImage(named: defaultIcon)
+		}
+
+		return UIImage.fontAwesomeIcon(
+			name: font,
+			style: SWIconConfig.style,
+			textColor: SWIconConfig.color,
+			size: size
+		)
 	}
 }
